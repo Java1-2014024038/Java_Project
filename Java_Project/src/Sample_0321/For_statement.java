4f2237aa0ee72168b639462f5e0487476cd4a941
basic <basic@DFC4FGY1>
2016-03-21 21:01:15 +0900

    for_statement and selection

---
+++ Java_Project/src/Sample_0321/For_statement.java
@@ -0,0 +1,13 @@
+package Sample_0321;
+
+public class For_statement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0 ; i < 10 ; i++){
+			System.out.println(" value : "+i);
+		}
+	}
+
+}
